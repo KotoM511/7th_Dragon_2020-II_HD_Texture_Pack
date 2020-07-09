765dacd2367478af95aed565449d90df0354bc80
KotoM511 <38396179+KotoM511@users.noreply.github.com>
2020-07-10 00:08:39 +1000

    Initial commit

---
+++ UI/menu/quest/File_Names.cmd
@@ -0,0 +1,5 @@
+@echo Grubbing rubbing all the file names in this folder, please wait...
+
+dir /b /A-D > filenames.txt
+
+@echo Done! Check filenames.txt and get to organizing your crap! You may not even be able to read this really bad echo text!
